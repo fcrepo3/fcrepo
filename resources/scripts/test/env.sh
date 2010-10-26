accf864428091af66468218cd114819b9fc33904
Chris Wilper <cwilper@duraspace.org>
2010-10-26 03:09:46 +0000

    Updated working maintenance version to 3.4.2-SNAPSHOT and for CI env.sh script, set BUILD_HOME to "." unless it's already defined.

--- resources/scripts/test/env.sh
+++ resources/scripts/test/env.sh
@@ -17,7 +17,9 @@ JAVA6_HOME=/usr/lib/jvm/java-6-sun
 M2_HOME=/usr/share/maven2
 
 # Where is the Fedora source distribution to be tested?
-BUILD_HOME=$HOME/bamboo-agent-home/xml-data/build-dir/FCREPO-LINUXSAN
+if [ -z "$BUILD_HOME" ]; then
+  BUILD_HOME=.
+fi
 
 # Where should test instances of Fedora be installed?
 # This will be created and cleared out by test scripts as necessary.
