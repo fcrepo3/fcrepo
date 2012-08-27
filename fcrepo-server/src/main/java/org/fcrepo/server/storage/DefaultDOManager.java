2c0929b0409183c20554c1519fdecb9067a8a556
fasseg <frank.asseg@googlemail.com>
2012-08-27 20:10:16 +0200

    FCREPO-1058: DOReaderCache Cleanup
    Cleanup of DOReaderCache since it used a pecualiar type unsafe logic to
    store cache entries. Changed implentation to use type safe list implementations
    and a spring managed thread in order to be JEE compliant
    
    fixed log statements to use slf4j's new {} syntax

--- fcrepo-server/src/main/java/org/fcrepo/server/storage/DefaultDOManager.java
+++ fcrepo-server/src/main/java/org/fcrepo/server/storage/DefaultDOManager.java
@@ -269,11 +269,6 @@ public class DefaultDOManager extends Module implements DOManager {
                             e.getMessage(), getRole());
         }
 
-        if (readerCacheSize > 0) {
-            m_readerCache =
-                    new DOReaderCache(readerCacheSize, readerCacheSeconds);
-        }
-
         // configuration of ingest validation
         String ingestValidationLevel = getParameter("ingestValidationLevel");
         if (ingestValidationLevel == null) {
@@ -418,6 +413,9 @@ public class DefaultDOManager extends Module implements DOManager {
             throw new ModuleInitializationException(
                     "LowlevelStorage not loaded", getRole());
         }
+        // get ref to DOReaderCache module
+        m_readerCache = (DOReaderCache) getServer().getBean("org.fcrepo.server.readerCache");
+        
         // get ref to FedoraStorageHintProvider
         try {
             m_hintProvider =
@@ -591,9 +589,6 @@ public class DefaultDOManager extends Module implements DOManager {
 
     @Override
     public void shutdownModule() {
-        if (m_readerCache != null) {
-            m_readerCache.close();
-        }
     }
 
     @Override
