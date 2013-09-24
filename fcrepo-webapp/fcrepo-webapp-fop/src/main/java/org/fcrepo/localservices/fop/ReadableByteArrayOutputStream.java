7d74cd42066ba0e67824719a353203526248df9d
Benjamin Armintor <armintor@gmail.com>
2013-09-24 00:51:27 -0400

    avoid copying byte arrays in FOP processing

---
+++ fcrepo-webapp/fcrepo-webapp-fop/src/main/java/org/fcrepo/localservices/fop/ReadableByteArrayOutputStream.java
@@ -0,0 +1,64 @@
+package org.fcrepo.localservices.fop;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetEncoder;
+
+
+public class ReadableByteArrayOutputStream extends ByteArrayOutputStream {
+
+    boolean closed;
+    
+    public ReadableByteArrayOutputStream() {
+        super();
+        closed = false;
+    }
+    
+    public ReadableByteArrayOutputStream(int size) {
+        super(size);
+        closed = false;
+    }
+    
+    @Override
+    public void write(byte[] b, int off, int len) {
+        if (closed) {
+            throw new IllegalStateException("ReadableByteArrayOutputStream has been closed for reading");
+        }
+        super.write(b, off, len);
+    }
+    
+    @Override
+    public void write(int b) {
+        if (closed) {
+            throw new IllegalStateException("ReadableByteArrayOutputStream has been closed for reading");
+        }
+        super.write(b);
+    }
+    
+    @Override
+    public void close() {
+        closed = true;
+    }
+    
+    public int length() {
+        return count;
+    }
+    
+    public void writeAllTo(OutputStream out) throws IOException {
+        out.write(buf, 0, count);
+    }
+    
+    public ByteArrayInputStream toInputStream() {
+        closed = true;
+        return new ByteArrayInputStream(buf, 0, count);
+    }
+    
+    public String getString(Charset cs) {
+        closed = true;
+        return new String(buf, 0, count, cs);
+    }
+    
+}
